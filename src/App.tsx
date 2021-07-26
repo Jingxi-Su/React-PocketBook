8c68caa23d09747379360e2eaaf0177b3f7023e5
jingxi <774874572@qq.com>
2021-07-26 17:23:24 +0800

    创建Layout组建

--- src/App.tsx
+++ src/App.tsx
@@ -5,63 +5,56 @@ import {
     Route,
     Redirect
 } from "react-router-dom";
-import styled from "styled-components";
-import Nav from "./components/Nav";
-
-const Wrapper = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-`;
-
-const Main = styled.div`
-  flex-grow: 1;
-  overflow: auto;
-`;
-
+import Layout from "./components/Layout";
 
 function App() {
     return (
         <Router>
-            <Wrapper>
-                <Main>
-                    <Switch>
-                        <Route path="/tags">
-                            <Tags/>
-                        </Route>
-                        <Route path="/money">
-                            <Money/>
-                        </Route>
-                        <Route path="/statistics">
-                            <Statistics/>
-                        </Route>
-                        <Redirect exact from={"/"} to={"/money"}/>//默认路由
-                        <Route path="*">
-                            <Nomatch/>
-                        </Route>
-                    </Switch>
-                </Main>
-
-                <Nav/>
-            </Wrapper>
+            <Switch>
+                <Route path="/tags">
+                    <Tags/>
+                </Route>
+                <Route path="/money">
+                    <Money/>
+                </Route>
+                <Route path="/statistics">
+                    <Statistics/>
+                </Route>
+                <Redirect exact from={"/"} to={"/money"}/>//默认路由
+                <Route path="*">
+                    <Nomatch/>
+                </Route>
+            </Switch>
         </Router>
     );
 }
 
 function Nomatch() {
-    return <div>页面不存在</div>;
+    return <div>页面不存在</div>
 }
 
 function Statistics() {
-    return <h2>统计页面</h2>;
+    return (
+        <Layout>
+            <h2>统计页面</h2>
+        </Layout>
+    )
 }
 
 function Tags() {
-    return <h2>标签页面</h2>;
+    return (
+        <Layout>
+            <h2>标签页面</h2>
+        </Layout>
+    )
 }
 
 function Money() {
-    return <h2>记账页面</h2>;
+    return (
+        <Layout>
+            <h2>记账页面</h2>
+        </Layout>
+    )
 }
 
 export default App;
