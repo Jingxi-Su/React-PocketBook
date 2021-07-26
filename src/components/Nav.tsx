5797aa15d3ab87674208393f1a723b7a3f31832f
jingxi <774874572@qq.com>
2021-07-26 17:06:55 +0800

    封装icons

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,7 @@
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 import React from "react";
-require('icons/money.svg');
-require('icons/tag.svg')
-require('icons/statistic.svg')
-
+import Icon from "./Icon";
 
 const NavWrapper = styled.nav`
   line-height: 24px;
@@ -21,7 +18,8 @@ const NavWrapper = styled.nav`
       flex-direction: column;
       align-items: center;
       justify-content: center;
-      .icon{
+
+      .icon {
         width: 24px;
         height: 24px;
       }
@@ -34,21 +32,15 @@ const Nav = () => {
         <NavWrapper>
             <ul>
                 <li>
-                    <svg className="icon">
-                        <use xlinkHref="#tag"/>
-                    </svg>
+                    <Icon name="tag"/>
                     <Link to="/tags">标签页</Link>
                 </li>
                 <li>
-                    <svg className="icon">
-                        <use xlinkHref="#money"/>
-                    </svg>
+                    <Icon name="money"/>
                     <Link to="/money">记账页</Link>
                 </li>
                 <li>
-                    <svg className="icon">
-                        <use xlinkHref="#statistic"/>
-                    </svg>
+                    <Icon name="statistic"/>
                     <Link to="/statistics">统计页</Link>
                 </li>
             </ul>
