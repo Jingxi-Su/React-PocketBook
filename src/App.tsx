305897728509c43200e1981beded6589af213f17
jingxi <774874572@qq.com>
2021-07-26 16:09:24 +0800

    项目搭建-icons添加成功

---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import {
+    HashRouter as Router,
+    Switch,
+    Route,
+    Redirect
+} from "react-router-dom";
+import styled from "styled-components";
+import Nav from "./components/Nav";
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`;
+
+
+function App() {
+    return (
+        <Router>
+            <Wrapper>
+                <Main>
+                    <Switch>
+                        <Route path="/tags">
+                            <Tags/>
+                        </Route>
+                        <Route path="/money">
+                            <Money/>
+                        </Route>
+                        <Route path="/statistics">
+                            <Statistics/>
+                        </Route>
+                        <Redirect exact from={"/"} to={"/money"}/>//默认路由
+                        <Route path="*">
+                            <Nomatch/>
+                        </Route>
+                    </Switch>
+                </Main>
+
+                <Nav/>
+            </Wrapper>
+        </Router>
+    );
+}
+
+function Nomatch() {
+    return <div>页面不存在</div>;
+}
+
+function Statistics() {
+    return <h2>统计页面</h2>;
+}
+
+function Tags() {
+    return <h2>标签页面</h2>;
+}
+
+function Money() {
+    return <h2>记账页面</h2>;
+}
+
+export default App;
