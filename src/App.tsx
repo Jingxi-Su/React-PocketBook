80d5129f2255bed108d0bcd7f4878bb358c2eeb1
jingxi <774874572@qq.com>
2021-07-28 23:58:36 +0800

    Money.tsx/TagsSection UI完成

--- src/App.tsx
+++ src/App.tsx
@@ -9,26 +9,33 @@ import Money from "./views/Money";
 import Statistics from "./views/Statistics";
 import Tags from "./views/Tags";
 import NoMatch from "./views/NoMatch";
+import styled from "styled-components";
+
+const AppWrapper = styled.div`
+  color: #333;
+`
 
 function App() {
     return (
-        <Router>
-            <Switch>
-                <Route path="/tags">
-                    <Tags/>
-                </Route>
-                <Route path="/money">
-                    <Money/>
-                </Route>
-                <Route path="/statistics">
-                    <Statistics/>
-                </Route>
-                <Redirect exact from={"/"} to={"/money"}/>//默认路由
-                <Route path="*">
-                    <NoMatch/>
-                </Route>
-            </Switch>
-        </Router>
+        <AppWrapper>
+            <Router>
+                <Switch>
+                    <Route path="/tags">
+                        <Tags/>
+                    </Route>
+                    <Route path="/money">
+                        <Money/>
+                    </Route>
+                    <Route path="/statistics">
+                        <Statistics/>
+                    </Route>
+                    <Redirect exact from={"/"} to={"/money"}/>//默认路由
+                    <Route path="*">
+                        <NoMatch/>
+                    </Route>
+                </Switch>
+            </Router>
+        </AppWrapper>
     );
 }
 
