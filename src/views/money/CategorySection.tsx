bbf402e77be672adef9ae7e89c496ff993c1f456
jingxi <774874572@qq.com>
2021-08-02 17:45:33 +0800

    完成Category切换功能

--- src/views/money/CategorySection.tsx
+++ src/views/money/CategorySection.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+import React, {useState} from "react";
 
-const CategorySection = styled.section`
+const Wrapper = styled.section`
   font-size: 24px;
 
   > ul {
@@ -19,5 +20,25 @@ const CategorySection = styled.section`
   }
 
 `
+
+const CategorySection: React.FunctionComponent = () => {
+    const categoryMap={'-':'支出','+':'收入'}
+    type Keys=keyof typeof categoryMap
+    const [categoryList] = useState<Keys[]>(['-', '+'])
+    //相当于const [categoryList] = useState<('-'|'+')[]>(['-', '+'])
+    const [category, setCategory] = useState('-')
+    return (
+        <Wrapper>
+            <ul>
+                {categoryList.map(c=>
+                    <li className={category === c ? 'selected' : ''}
+                        onClick={() => setCategory(c)}>
+                        {categoryMap[c]}
+                    </li>
+                )}
+            </ul>
+        </Wrapper>
+    )
+}
 
 export {CategorySection}
