31fb3cdda651dcdd36b568abad935b938d23a1cb
jingxi <774874572@qq.com>
2021-08-07 14:57:14 +0800

    taglist样式做完

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,15 +1,61 @@
 import Layout from "../components/Layout";
 import React from "react";
 import {useTags} from "../useTags";
+import styled from "styled-components";
+import Icon from "components/Icon";
+
+const Taglist = styled.ol`
+  font-size: 16px;
+  background: white;
+
+  > li {
+    border-bottom: 1px solid #d5d5d9;
+    line-height: 20px;
+    padding: 12px 16px 12px 0;
+    margin-left: 16px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`
+
+const Button = styled.button`
+  font-size: 18px;
+  border: none;
+  padding: 8px 12px;
+  background: #ffd833;
+  border-radius: 4px;
+`
+
+const Center=styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+
+const Space=styled.div`
+    height: 16px;
+`
 
 function Tags() {
     const {tags, setTags} = useTags();//析构
     return (
         <Layout>
-            <ol>
-                {tags.map(tag=>
-                <li key={tag}>{tag}</li>)}
-            </ol>
+            <Taglist>
+                {tags.map(tag =>
+                    <li key={tag}>
+                        <span className="oneLine">{tag}</span>
+                        <Icon name="right"/>
+                    </li>)}
+            </Taglist>
+            <Center>
+                <Space/>
+                <Space/>
+                <Space/>
+                <Button>新增标签</Button>
+                <Space/>
+            </Center>
         </Layout>
     )
 }
