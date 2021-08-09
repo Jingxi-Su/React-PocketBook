cd001b6a3758ff15107ade7e6a8b54cbb694b91c
jingxi <774874572@qq.com>
2021-08-09 17:51:40 +0800

    封装findTag到useTags

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,14 +1,17 @@
 import {useState} from "react";
 import {createId} from "./lib/createId";
 
+const defaultTags = [//防止每次调用，id都会重新生成
+    {id: createId(), name: '衣'},
+    {id: createId(), name: '食'},
+    {id: createId(), name: '住'},
+    {id: createId(), name: '行'}
+]
+
 const useTags = () => {//自定义hook必须use开头
-    const [tags, setTags] = useState<{ id: number; name: string }[]>([
-        {id: createId(), name: '衣'},
-        {id: createId(), name: '食'},
-        {id: createId(), name: '住'},
-        {id: createId(), name: '行'}
-    ]);
-    return {tags, setTags}//后期可以研究为什么必须导出对象而不是数组
+    const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
+    const findTag = (id: number) => tags.filter(tag => tag.id === id)[0];
+    return {tags, setTags, findTag}//后期可以研究为什么必须导出对象而不是数组
 }
 
 export {useTags}
--- src/views/TagEdit.tsx
+++ src/views/TagEdit.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import {useTags} from "../useTags";
+import {useParams} from "react-router-dom";
 
-const TagEdit:React.FunctionComponent=()=>{
-    return(
-        <div>hi</div>
+type Params={
+    id:string
+}
+const TagEdit: React.FunctionComponent = () => {
+    const {findTag} = useTags()
+    let {id} = useParams<Params>();
+    const tag = findTag(parseInt(id))
+    return (
+        <div>{tag.name}</div>
     )
 }
 
