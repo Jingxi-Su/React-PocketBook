f9b2e193ac175902126ff9aabf85b11b415512da
jingxi <774874572@qq.com>
2021-08-10 21:02:25 +0800

    useTags优化更新和删除功能

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -22,24 +22,10 @@ const useTags = () => {//自定义hook必须use开头
         return result;
     }
     const updateTag = (id: number, obj: { name: string }) => {
-        //获取要修改的tag的下标
-        const index = findTagIndex(id);
-        //深拷贝tags
-        //vue可以直接在原数据上修改，但react不支持这项功能，因为它认为数据不可变
-        const tagsClone = JSON.parse(JSON.stringify(tags))
-        //把tagsClone的第index个删掉，换成{id: id, name: obj.name}
-        tagsClone.splice(index, 1, {id: id, name: obj.name})
-        setTags(tagsClone)
+        setTags(tags.map(tag => tag.id === id ? {id, name: obj.name} : tag));
     }
     const deleteTag = (id: number) => {
-        //获取要删除的tag的下标
-        const index = findTagIndex(id);
-        //深拷贝tags
-        //vue可以直接在原数据上修改，但react不支持这项功能，因为它认为数据不可变
-        const tagsClone = JSON.parse(JSON.stringify(tags))
-        //把tagsClone的第index个删掉
-        tagsClone.splice(index, 1)
-        setTags(tagsClone)
+        setTags(tags.filter(tag => tag.id !== id))
     }
     return {tags, setTags, findTag, updateTag, findTagIndex, deleteTag}//后期可以研究为什么必须导出对象而不是数组
 }
