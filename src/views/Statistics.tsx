688adaa75d79332662cdaed7a87a274804b44e96
jingxi <774874572@qq.com>
2021-08-12 19:20:21 +0800

    展示界面格式梅花

--- src/views/Statistics.tsx
+++ src/views/Statistics.tsx
@@ -4,24 +4,40 @@ import {CategorySection} from "./money/CategorySection";
 import {useRecords} from "../hooks/useRecords";
 import {useTags} from "../hooks/useTags";
 import day from "dayjs";
+import styled from "styled-components";
+
+const Item = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background: white;
+  font-size: 18px;
+  line-height: 20px;
+  padding: 10px 16px;
+  >.note{
+    margin-right: auto;
+    margin-left: 16px;
+    color: #999;
+  }
+`
 
 function Statistics() {
-    const [category,setCategory]=useState<'-'|'+'>('-')
-    const {records}=useRecords()
-    const {getName}=useTags()
+    const [category, setCategory] = useState<'-' | '+'>('-')
+    const {records} = useRecords()
+    const {getName} = useTags()
     return (
         <Layout>
             <CategorySection value={category}
                              onChange={(value) => setCategory(value)}/>
             <div>
-                {records.map(r=>{
-                    return <div>
-                        {r.tagIds.map(tagId=><span>{getName(tagId)}</span>)}
-                        <hr/>
-                        {r.amount}
-                        <hr/>
-                        {day(r.createdAt).format('YYYY年MM月DD日')}
-                    </div>
+                {records.map(r => {
+                    return <Item>
+                        <div className="tags">
+                            {r.tagIds.map(tagId => <span>{getName(tagId)}</span>)}
+                        </div>
+                        {r.note && <div className="note">{r.note}</div>}
+                        <div className="amount">￥{r.amount}</div>
+                        {/*{day(r.createdAt).format('YYYY年MM月DD日')}*/}
+                    </Item>
                 })}
             </div>
         </Layout>
