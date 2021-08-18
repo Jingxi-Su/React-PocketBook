dfc2cd0f94869fa5a7e2dd2bd974a4b9a55721d0
jingxi <774874572@qq.com>
2021-08-18 16:02:31 +0800

    优化滚动逻辑

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import Nav from "./Nav";
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
@@ -13,15 +13,33 @@ const Main = styled.div`
   overflow: auto;
 `;
 
-const Layout = (props: any) => {
+type Props = {
+    className?: string
+    scrollTop?: number//问号表示该参数可以不传
+}
+
+const Layout: React.FunctionComponent<Props> = (props) => {
+    const mainRef = useRef<HTMLDivElement>(null)
+    useEffect(() => {
+        setTimeout(()=>{
+            if (!mainRef.current) {
+                return
+            }
+            mainRef.current.scrollTop = props.scrollTop!//感叹号表示该参数不可能为空
+        },0)//设置延迟是因为刚进入页面时滚动条还未加载出来，本代码可能无效。（0表示尽快）
+    }, [props.scrollTop])
     return (
         <Wrapper>
-            <Main className={props.className}>
+            <Main ref={mainRef} className={props.className}>
                 {props.children}
             </Main>
             <Nav/>
         </Wrapper>
     )
+}
+
+Layout.defaultProps = {//默认参数设置
+    scrollTop: 0
 }
 
 export default Layout;
